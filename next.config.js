18a28c247559d4ff9818a39582fe192711ab83a4
Jerom Verschoote <33222896+jeromverschoote@users.noreply.github.com>
2021-05-05 14:54:13 +0200

    :wrench: Add config files for Babel, IDE, GitHub, Prettier, VSCode and Next

---
+++ next.config.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const runtimeCaching = require('next-pwa/cache');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching: runtimeCaching,
+    disable: process.env.NODE_ENV === 'production' ? false : true,
+  },
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['nl-BE', 'en-US'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'nl-BE',
+  },
+  images: {
+    domains: [
+      'images.pexels.com',
+      'images.unsplash.com',
+      'upload.wikimedia.org',
+    ],
+  },
+});
